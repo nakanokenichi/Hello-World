159c0cdf0944cfdfcce80e69df8732d06a2ca19a
nakanokenichi <nakanok@solutionware.jp>
2015-10-06 15:49:48 +0900

    first commit (add file)

---
+++ var/www/html/canvas/template/canvas_graph.php
@@ -0,0 +1,12 @@
+<div id="gadgetDialog">
+  <p>{{dialog_title}}</p>
+  <div class="col-md-12">
+    <div id="expandGraph" class="col-md-12">
+    </div>
+  </div>
+  <div class="row">
+    <div style="margin-top: 15px;" class="col-md-12" align="right">
+      <a class="back_button" ng-click="closeExpandDialog()">閉じる</a>
+      </div>
+  </div>
+</div>
